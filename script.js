cd67c0df10cb13ddfbde94cfaae626ef23a55b4b
Pedro <pedropaivadev@gmail.com>
2023-04-15 15:13:07 -0300

    exercice 201: typing the function parameter and correcting the name of the methods

--- script.js
+++ script.js
@@ -1,21 +1,5 @@
 "use strict";
-const produto = 'Livro';
-const nintendo = {
-    nome: 'Nintendo',
-    preco: '2000',
-};
-const preco = 200;
-const barato = preco < 400 ? true : "produto caro";
-const carro = {
-    marca: 'Audi',
-    portas: 5,
-};
-function somar(a, b) {
-    return a + b;
+function normalizarTexto(texto) {
+    return texto.trim().toLowerCase();
 }
-function transformarPreco(produto) {
-    produto.preco = 'R$ ' + produto.preco;
-    return produto;
-}
-console.log(somar(4, 10));
-console.log(transformarPreco(nintendo));
+console.log(normalizarTexto(' Pedro Paiva '));
--- script.ts
+++ script.ts
@@ -1,27 +1,5 @@
-const produto: string = 'Livro';
-const nintendo = {
-  nome: 'Nintendo',
-  preco: '2000',
-}
-const preco: number = 200;
-const barato = preco < 400 ? true : "produto caro";
-
-const carro: {
-  marca: string;
-  portas: number;
-} = {
-  marca: 'Audi',
-  portas: 5,
-};
-
-function somar(a:number, b:number) {
-  return a + b;
+function normalizarTexto(texto:string) {
+  return texto.trim().toLowerCase();
 }
 
-function transformarPreco(produto: {nome:string; preco:string}) {
-  produto.preco = 'R$ ' + produto.preco;
-  return produto;
-}
-
-console.log(somar(4,10));
-console.log(transformarPreco(nintendo));
+console.log(normalizarTexto(' Pedro Paiva '))
