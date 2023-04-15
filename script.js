53969216c186cfd76e6c0597664102097886e20f
Pedro <pedropaivadev@gmail.com>
2023-04-15 14:52:51 -0300

    class 201: annotation and type inference

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 "use strict";
-const total = 100;
+const produto = 'Livro';
+const nintendo = {
+    nome: 'Nintendo',
+    preco: '2000',
+};
+const preco = 200;
+const barato = preco < 400 ? true : "produto caro";
+const carro = {
+    marca: 'Audi',
+    portas: 5,
+};
 function somar(a, b) {
     return a + b;
 }
-console.log(somar(5, 10));
+function transformarPreco(produto) {
+    produto.preco = 'R$ ' + produto.preco;
+    return produto;
+}
+console.log(somar(4, 10));
+console.log(transformarPreco(nintendo));
--- script.ts
+++ script.ts
@@ -1,7 +1,27 @@
-const total = 100;
+const produto: string = 'Livro';
+const nintendo = {
+  nome: 'Nintendo',
+  preco: '2000',
+}
+const preco: number = 200;
+const barato = preco < 400 ? true : "produto caro";
 
-function somar(a: number, b: number) {
-  return a + b
+const carro: {
+  marca: string;
+  portas: number;
+} = {
+  marca: 'Audi',
+  portas: 5,
+};
+
+function somar(a:number, b:number) {
+  return a + b;
 }
 
-console.log(somar(5, 10));
+function transformarPreco(produto: {nome:string; preco:string}) {
+  produto.preco = 'R$ ' + produto.preco;
+  return produto;
+}
+
+console.log(somar(4,10));
+console.log(transformarPreco(nintendo));
