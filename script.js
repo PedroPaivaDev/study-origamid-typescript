829612c1c83cd5967feb5284699cd0e5f4dc6b9d
Pedro <pedropaivadev@gmail.com>
2023-04-16 11:42:21 -0300

    exercise 204: fetch data from the api, make an interface for the received data and show them on screen

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 "use strict";
-function preencherDados(dados) {
-    document.body.innerHTML += `
-  <div>
-    <h2>${dados.nome}</h2>
-    <p>R$ ${dados.preco}</p>
-    <p>Inclui teclado: ${dados.teclado ? 'sim' : 'não'}</p>
-  </div>
+async function fetchProduct() {
+    const response = await fetch('https://api.origamid.dev/json/notebook.json');
+    const data = await response.json();
+    showProduct(data);
+}
+fetchProduct();
+function showProduct(data) {
+    document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <h3>R$ ${data.preco.toFixed(2)}</h3>
+      <p>${data.descricao}</p>
+      <p>${data.garantia} anos de garantia! </p>
+      ${data.seguroAcidentes && '<spam>Possui seguro em caso de acidentes</spam>'}
+      <p>Fabricante: ${data.empresaFabricante.nome}, ${data.empresaFabricante.fundacao}, ${data.empresaFabricante.pais}</p>
+      <p>Montadora: ${data.empresaMontadora.nome}, ${data.empresaMontadora.fundacao}, ${data.empresaMontadora.pais}</p>
+    </div>
   `;
 }
-const computador = {
-    nome: 'Computador',
-    preco: 2000,
-    teclado: false,
-};
-preencherDados(computador);
-preencherDados({
-    nome: 'Notebook',
-    preco: 2500,
-    teclado: true,
-});
--- script.ts
+++ script.ts
@@ -1,32 +1,37 @@
-interface Produto {
-  nome: string;
-  preco: number;
-  teclado: boolean;
+interface Empresa {
+  nome: string,
+  fundacao: number,
+  pais: string
 }
 
-function preencherDados(dados: Produto) {
-  document.body.innerHTML += `
-  <div>
-    <h2>${dados.nome}</h2>
-    <p>R$ ${dados.preco}</p>
-    <p>Inclui teclado: ${dados.teclado ? 'sim' : 'não'}</p>
-  </div>
+interface Produto {
+  nome: string,
+  preco: number,
+  descricao: string,
+  garantia: string,
+  seguroAcidentes: boolean,
+  empresaFabricante: Empresa,
+  empresaMontadora: Empresa
+}
+
+async function fetchProduct() {
+  const response = await fetch('https://api.origamid.dev/json/notebook.json');
+  const data = await response.json();
+  showProduct(data);
+}
+
+fetchProduct();
+
+function showProduct(data:Produto) {
+  document.body.innerHTML = `
+    <div>
+      <h2>${data.nome}</h2>
+      <h3>R$ ${data.preco.toFixed(2)}</h3>
+      <p>${data.descricao}</p>
+      <p>${data.garantia} anos de garantia! </p>
+      ${data.seguroAcidentes && '<spam>Possui seguro em caso de acidentes</spam>'}
+      <p>Fabricante: ${data.empresaFabricante.nome}, ${data.empresaFabricante.fundacao}, ${data.empresaFabricante.pais}</p>
+      <p>Montadora: ${data.empresaMontadora.nome}, ${data.empresaMontadora.fundacao}, ${data.empresaMontadora.pais}</p>
+    </div>
   `;
 }
-
-const computador: Produto = {
-  nome: 'Computador',
-  preco: 2000,
-  teclado: false,
-}
-
-preencherDados(computador);
-
-preencherDados({
-  nome: 'Notebook',
-  preco: 2500,
-  teclado: true,
-});
-
-type NumberOrString = number | string; //posso definir tipos primitivos
-type Categorias = 'design' | 'codigo' | 'descod';// posso definir também strings como tipos
