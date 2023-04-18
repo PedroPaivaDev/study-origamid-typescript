cd97321634858c3093e6b56acf88fae52d237316
Pedro <pedropaivadev@gmail.com>
2023-04-18 10:08:43 -0300

    class 302-2: querySelectorAll returns a list of nodes, but it doesn't make it possible to use array methods. To use the array methods, it is necessary to condition the instanceof or actually turn it into an array.

--- script.ts
+++ script.ts
@@ -1,12 +1,22 @@
-const video = document.querySelector('video'); // HTMLVideoElement
-const videoID = document.querySelector('#videoPrincipal'); // HTMLElement
+const links = document.querySelectorAll('.link');
 
-const link1 = document.querySelector('a'); // HTMLAnchorElement
-const link2 = document.querySelector('#origamid'); // Element
+links.forEach((link) => {
+  if (link instanceof HTMLAnchorElement) {
+    console.log(link.href); //retorna o href dos dois elementos do tipo 'anchor' dentro da variável 'links'
+  } else {
+    console.log("não é 'anchor'", typeof link); //nesse caso, o 'typeof' sempre vai retornar 'object', pois ele só funcina direito com tipos primitvos.
+  }
+});
 
-video?.volume;
-// videoID?.volume; // erro no ts
-if(videoID instanceof HTMLVideoElement) console.log(videoID?.volume)
+console.log('NodeList', links instanceof NodeList);
 
-link1?.href;
-// link2?.href; // erro no ts
+// erro, filter é um método de Array e não de NodeList
+// const anchorLinksTeste1 = links.filter((link) => link instanceof HTMLAnchorElement);
+if(links instanceof Array) {links.filter(link => link instanceof HTMLAnchorElement)} //assim funciona
+
+// Assim também funciona, pois a NodeList foi transformada em uma Array, antes de ser feito o filter
+const anchorLinks = Array.from(links).filter(
+  (link) => link instanceof HTMLAnchorElement,
+);
+console.log('links', links) //retorna um array inteiro com 3 objetos, dois tipo 'anchor' e um tipo 'button'
+console.log('array', anchorLinks) //retorna um array filtrado com 2 objetos do tipo 'anchor'
