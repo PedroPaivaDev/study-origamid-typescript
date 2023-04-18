b8971a734e4b551baada60536206e5055bfc3dee
Pedro <pedropaivadev@gmail.com>
2023-04-18 09:26:44 -0300

    class 302-1: using instanceof to wrap a DOM element in the inherited class's typing and then be able to use its methods

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 "use strict";
-//Exercício feito:
-const link = document.getElementById('origamid');
-const href = link?.getAttribute('href');
-function changeURL() {
-    const dotsIndex = href?.indexOf(':');
-    if (dotsIndex) {
-        return href?.slice(0, dotsIndex) + 's' + href?.slice(dotsIndex);
-    }
-}
-console.log('exercício', changeURL());
-//Resolução:
-console.dir(link); //retorna o objeto 'anchor' do DOM.
-//O prototype de 'anchor' é a classe 'HTMLAnchorElement' e o prototype de 'HTMLAnchorElement' é 'HTMLElement'. Então em algum lugar do JavaScript, foi escrito: class HTMLAnchorElement extends HTMLElement, pois o 'anchor' foi feito com base na classe HTMLAnchorElement e a HTMLAnchorElement foi feito com base na classe HTMLElement.
-console.log(link instanceof HTMLAnchorElement); //true
-if (link instanceof HTMLAnchorElement) {
-    // link.href = "https://www.origamid.com";
-    //ou
-    link.href = link.href.replace("http://", "https://");
-    console.log('resolução', link.href);
-}
+const video = document.querySelector('video'); // HTMLVideoElement
+const videoID = document.querySelector('#videoPrincipal'); // HTMLElement
+const link1 = document.querySelector('a'); // HTMLAnchorElement
+const link2 = document.querySelector('#origamid'); // Element
+video?.volume;
+// videoID?.volume; // erro no ts
+if (videoID instanceof HTMLVideoElement)
+    console.log(videoID?.volume);
+link1?.href;
+// link2?.href; // erro no ts
