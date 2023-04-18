2c5277726dcea3876a749a2bf03940c661e75d26
Pedro <pedropaivadev@gmail.com>
2023-04-18 14:53:55 -0300

    exercise 303: change menu-hamburger class and change menu-display button aria-props

--- script.js
+++ script.js
@@ -1,14 +1,52 @@
 "use strict";
-const button = document.querySelector('button');
-// function ativarMenu(this: HTMLButtonElement, event: MouseEvent) {
-//   console.log(this); //o 'this' dentro de um callback(o eventListener), faz referência ao objeto que ele foi chamado, no caso, referente ao 'button'.
+// Estado dos elementos
+// menu inativo:
+// class="" em nav
+// aria-expanded="false" em button
+// aria-label="Abrir Menu" em button
+// menu ativo:
+// class="active" em nav
+// aria-expanded="true" em button
+// aria-label="Fechar Menu" em button
+//---------------------
+//exercício feito:
+// const navBar = document.querySelector('#nav');
+// const button = document.querySelector('#btn-mobile');
+// function handleClick(event:Event) {
+//   if(navBar?.className==='active') {
+//     navBar?.classList.remove("active");
+//     if(event.currentTarget instanceof HTMLButtonElement) {
+//       event.currentTarget.ariaExpanded = 'false';
+//       event.currentTarget.ariaLabel = 'Abrir Menu'
+//     }
+//   } else {
+//     navBar?.classList.add("active");
+//     if(event.currentTarget instanceof HTMLButtonElement) {
+//       event.currentTarget.ariaExpanded = 'true';
+//       event.currentTarget.ariaLabel = 'Fechar Menu'
+//     }
+//   }
 // }
-function ativarMenu(event) {
-    const elemento = event.currentTarget;
-    if (elemento instanceof HTMLElement) {
-        elemento.style.background = 'red';
+// button?.addEventListener('click', handleClick);
+//---------------------
+//resolução professor:
+const btnMobile = document.getElementById("btn-mobile");
+function toggleMenu(event) {
+    const nav = document.getElementById("nav");
+    const button = event.currentTarget;
+    if (button instanceof HTMLElement && nav) {
+        const active = nav.classList.contains("active");
+        if (active) {
+            nav.classList.remove("active");
+            button.setAttribute("aria-expanded", "false");
+            button.setAttribute("aria-label", "Abrir Menu");
+        }
+        else {
+            nav.classList.add("active");
+            button.setAttribute("aria-expanded", "true");
+            button.setAttribute("aria-label", "Fechar Menu");
+        }
     }
-    console.log(elemento);
+    console.log(event);
 }
-button?.addEventListener('click', ativarMenu);
-window.addEventListener('keydown', ativarMenu);
+btnMobile?.addEventListener("pointerdown", toggleMenu);
