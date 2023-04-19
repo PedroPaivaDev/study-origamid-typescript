c146035413b19d4de7a6f5c755492442bbffadb8
Pedro <pedropaivadev@gmail.com>
2023-04-19 13:54:16 -0300

    class 304-3: defining the type in the call of native methods

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 "use strict";
-// Exemplo 1
-function extractText(el) {
-    return el.innerText;
+// Define que o retorno será um HTMLAnchorElement
+const link = document.querySelector('.link');
+link?.href;
+//CUIDADO! Definir o tipo em métodos nativos pode gerar erros, caso seja feita uma tipagem com uma classe errada.
+//A forma mais segura é verificar antes com o instanceof
+if (link instanceof HTMLVideoElement) {
+    link?.volume;
+} //aqui mostra os métodos possíveis
+async function getData(url) {
+    const response = await fetch(url);
+    return await response.json();
+} // agora dentro da Promise, será retornado um objeto do tipo definido na interface Notebook
+async function handleData() {
+    const notebook = await getData('https://api.origamid.dev/json/notebook.json');
+    console.log(notebook.nome);
 }
-//Esse <Tipo extends HTMLElement = HTMLElement> quer dizer que se não passar nenhum tipo, então usar como padrão o HTMLElement
-const link = document.querySelector('a');
-if (link) {
-    console.log(extractText(link));
-    // extractText<HTMLAnchorElement extends HTMLElement>(el: HTMLAnchorElement): string
-}
-// Exemplo 2: recriando um método do JQuery
-function $(selector) {
-    return document.querySelector(selector);
-}
-// ao invés de usar o querySelector toda vez, eu uso apenas:
-console.log($('a'));
-console.log($('a')?.href);
--- script.ts
+++ script.ts
@@ -1,21 +1,22 @@
-// Exemplo 1
-function extractText<Tipo extends HTMLElement = HTMLElement>(el: Tipo): string {
-  return el.innerText;
+// Define que o retorno será um HTMLAnchorElement
+const link = document.querySelector<HTMLAnchorElement>('.link');
+link?.href;
+//CUIDADO! Definir o tipo em métodos nativos pode gerar erros, caso seja feita uma tipagem com uma classe errada.
+//A forma mais segura é verificar antes com o instanceof
+if(link instanceof HTMLVideoElement) {link?.volume} //aqui mostra os métodos possíveis
+
+async function getData<T>(url: string): Promise<T> {
+  const response = await fetch(url);
+  return await response.json();
+}// agora dentro da Promise, será retornado um objeto do tipo definido na interface Notebook
+
+interface Notebook {
+  nome: string;
 }
 
-//Esse <Tipo extends HTMLElement = HTMLElement> quer dizer que se não passar nenhum tipo, então usar como padrão o HTMLElement
-
-const link = document.querySelector('a');
-
-if (link) {
-  console.log(extractText(link));
-  // extractText<HTMLAnchorElement extends HTMLElement>(el: HTMLAnchorElement): string
+async function handleData() {
+  const notebook = await getData<Notebook>(
+    'https://api.origamid.dev/json/notebook.json',
+  );
+  console.log(notebook.nome);
 }
-
-// Exemplo 2: recriando um método do JQuery
-function $<Tipo extends Element>(selector: string): Tipo | null {
-  return document.querySelector(selector);
-}
-// ao invés de usar o querySelector toda vez, eu uso apenas:
-console.log( $('a') );
-console.log( $<HTMLAnchorElement>('a')?.href )
