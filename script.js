c6810c9685a2553b414ac7adbad0e12617271d42
Pedro <pedropaivadev@gmail.com>
2023-04-19 17:52:01 -0300

    class 307: use type 'unknown' instead of 'any' to require the function to use typeGuards before returning a value

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 "use strict";
-//eu condiciono várias possibilidades de tipo para o seus métodos específicos
 function typeGuard(value) {
-    if (typeof value === 'string') {
+    if (typeof value === 'string') { //se não tiver esse condicional de 'typeof', o 'unkown' não permite que o uso de 'value'
         return value.toLowerCase();
     }
     if (typeof value === 'number') {
@@ -14,25 +13,3 @@ function typeGuard(value) {
 typeGuard('Origamid');
 typeGuard(200);
 typeGuard(document.body);
-//------------
-//outra forma de fazer typeGuard, é usando o método nativo do JavaScript "in"
-const obj = {
-    nome: 'Origamid',
-};
-if ('nome' in obj) { // 'nome' precisa ser passado como uma string, mesmo que seja uma chave
-    console.log('Sim');
-}
-async function fetchProduto() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    const json = await response.json();
-    handleProduto(json);
-}
-function handleProduto(data) {
-    if ('preco' in data) { //para garantir que o preço só vai aparecer em tela, se as propriedades baterem
-        document.body.innerHTML += `
-      <p>Nome: ${data.nome}</p>
-      <p>Preço: R$ ${data.preco + 100}</p>
-    `;
-    }
-}
-fetchProduto();
--- script.ts
+++ script.ts
@@ -1,6 +1,5 @@
-//eu condiciono várias possibilidades de tipo para o seus métodos específicos
-function typeGuard(value: any) {
-  if (typeof value === 'string') {
+function typeGuard(value: unknown) {
+  if (typeof value === 'string') {//se não tiver esse condicional de 'typeof', o 'unkown' não permite que o uso de 'value'
     return value.toLowerCase();
   }
   if (typeof value === 'number') {
@@ -13,31 +12,3 @@ function typeGuard(value: any) {
 typeGuard('Origamid');
 typeGuard(200);
 typeGuard(document.body);
-
-//------------
-//outra forma de fazer typeGuard, é usando o método nativo do JavaScript "in"
-const obj = {
-  nome: 'Origamid',
-};
-if ('nome' in obj) {// 'nome' precisa ser passado como uma string, mesmo que seja uma chave
-  console.log('Sim');
-}
-
-interface Produto {
-  nome: string;
-  preco: number;
-}
-async function fetchProduto() {
-  const response = await fetch('https://api.origamid.dev/json/notebook.json');
-  const json = await response.json();
-  handleProduto(json);
-}
-function handleProduto(data: Produto) {
-  if ('preco' in data) {//para garantir que o preço só vai aparecer em tela, se as propriedades baterem
-    document.body.innerHTML += `
-      <p>Nome: ${data.nome}</p>
-      <p>Preço: R$ ${data.preco + 100}</p>
-    `;
-  }
-}
-fetchProduto();
