c8ea866c6bf9dfdf980cd5ae07d6caa83c2fa333
Pedro <pedropaivadev@gmail.com>
2023-04-21 14:53:49 -0300

    exercise 312: I completed the exercise, but there was an error in the event listener callback

--- script.js
+++ script.js
@@ -1,23 +1,43 @@
 "use strict";
-function handleProdutoCarro(dados) {
-    dados.rodas;
-    dados.portas;
-    dados.preco;
+// 1 - Crie uma interface UserData para o formulário abaixo
+// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
+// 3 - Adicione um evento de keyup ao formulário
+// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
+// 5 - Salve UserData no localStorage
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
+const nome = document.getElementById('nome');
+const email = document.getElementById('email');
+const cpf = document.getElementById('cpf');
+const localUserData = JSON.parse(window.localStorage.getItem('UserData'));
+function handleKeyUp({ target }) {
+    const id = target.id;
+    const value = target.value;
+    window.UserData = {
+        ...window.UserData,
+        [id]: value
+    };
+    window.localStorage.setItem('UserData', JSON.stringify(window.UserData));
 }
-handleProdutoCarro({
-    preco: 20000,
-    rodas: 4,
-    portas: 5,
-});
-const dado1 = {
-    preco: 20000,
-    rodas: 4,
-    portas: 5,
-};
-const dado2 = {
-    preco: 20000,
-    rodas: 4,
-    portas: 5,
-};
-window.userId = 200;
-console.log(window.userId);
+function isUserData(data) {
+    if (data && typeof data === 'object' &&
+        ('nome' in data || 'email' in data || 'cpf' in data)
+    //não faz sentido perder todos os dados, só pq o usuário não preencheu todos os campos anteriormente, então eu acho melhor usar o 'ou'
+    ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function getUserData() {
+    if (isUserData(localUserData)) {
+        nome.value = localUserData.nome;
+        email.value = localUserData.email;
+        cpf.value = localUserData.cpf;
+        window.UserData = localUserData;
+    }
+}
+getUserData();
+document.documentElement.addEventListener('keyup', handleKeyUp);
+//só não consegui corrigir esse erro da handleKeyUp, pq ela precisa retornar void mesmo
--- script.ts
+++ script.ts
@@ -1,58 +1,60 @@
-//-------------
-//Intersection (&) só funciona usando type
-//para interface usamos o extends
-type Produto = {
-  preco: number;
-};
-type Carro = {
-  rodas: number;
-  portas: number;
-  preco: number;//não precisa declarar esse tipo, pois ele já está na interface 'Produto'
-};
-function handleProdutoCarro(dados: Carro & Produto) {
-  dados.rodas;
-  dados.portas;
-  dados.preco;
-}
-handleProdutoCarro({
-  preco: 20000,
-  rodas: 4,
-  portas: 5,
-});
+// 1 - Crie uma interface UserData para o formulário abaixo
+// 2 - Crie uma variável global UserData no window, ela será um objeto qualquer
+// 3 - Adicione um evento de keyup ao formulário
+// 4 - Quando o evento ocorrer adicione a {[id]: value} ao UserData
+// 5 - Salve UserData no localStorage
+// 6 - Crie uma User Type Guard, para verificar se o valor de localStorage é compatível com UserData
+// 7 - Ao refresh da página, preencha os valores de localStorage (caso seja UserData) no formulário e em window.UserData
 
-//-------------
-//Adicionando propriedades em interfaces e types:
-// Com Interface
-interface InterfaceCarro {
-  rodas: number;
-  portas: number;
+//-------------------------------------------------
+//EXERCÍCIO feito, mas com um problema a ser corrigido
+interface UserData {
+  nome: string;
+  email: string;
+  cpf: string;
 }
-interface InterfaceCarro {// basta repetir o nome da interface para adicionar uma propriedade
-  preco: number;
-}
-const dado1: InterfaceCarro = {
-  preco: 20000,
-  rodas: 4,
-  portas: 5,
-};
-// Com Type
-type TipoCarro = {
-  rodas: number;
-  portas: number;
-};
-type TipoCarroComPreco = TipoCarro & {//basta usar o intersection (&) para adicionar direto
-  preco: number;
-};
-const dado2: TipoCarroComPreco = {
-  preco: 20000,
-  rodas: 4,
-  portas: 5,
-};
 
-//-------------
-//window
-interface Window {// desse modo, eu posso usar a classe nativa Window, para ter acesso global à tipagem do 'id do usuário de um app'
-  userId: number;
+interface Window {
+  UserData: UserData;
 }
-window.userId = 200;
-console.log(window.userId);
+
+const nome = document.getElementById('nome') as HTMLInputElement;
+const email = document.getElementById('email') as HTMLInputElement;
+const cpf = document.getElementById('cpf') as HTMLInputElement;
+const localUserData = JSON.parse(window.localStorage.getItem('UserData') as string);
+
+function handleKeyUp({target}:{target:HTMLInputElement}) {
+  const id = target.id as keyof string;
+  const value = target.value;
+  window.UserData = {
+    ...window.UserData,
+    [id]: value
+  };
+  window.localStorage.setItem('UserData', JSON.stringify(window.UserData));
+}
+
+function isUserData(data: unknown): data is UserData {//typeGuard
+  if(
+    data && typeof data === 'object' &&
+    ('nome' in data || 'email' in data || 'cpf' in data)
+    //não faz sentido perder todos os dados, só pq o usuário não preencheu todos os campos anteriormente, então eu acho melhor usar o 'ou'
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function getUserData() {
+  if(isUserData(localUserData)) {
+    nome.value = localUserData.nome
+    email.value = localUserData.email
+    cpf.value = localUserData.cpf
+    window.UserData = localUserData;
+  }
+}
+
+getUserData();
+
+document.documentElement.addEventListener('keyup', handleKeyUp);
+//só não consegui corrigir esse erro da handleKeyUp, pq ela precisa retornar void mesmo
