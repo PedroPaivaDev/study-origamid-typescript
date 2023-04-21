11171407336a396a7b03cdd3140b55e2394ec14f
Pedro <pedropaivadev@gmail.com>
2023-04-21 20:14:00 -0300

    class 403: example of a generic function to check keys inside an object (using 'is' and 'in') and inside an interface (using 'keyof')

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 "use strict";
-let chave;
-// let chave: "nome" | "produto";
-chave = 'nome';
-chave = 'preco';
-//---------------
-//typeof é nativo do JS, mas tbm temos o do TS, que serve para declarar o tipo
-function coordenadas(x, y) {
-    return { x, y };
+async function fetchData(url) {
+    const base = 'https://api.origamid.dev/json';
+    const response = await fetch(base + url);
+    return await response.json();
 }
-let coord; //nesse caso, 'coord' virou uma arrow function auutomaticamente
-coord = (x, y) => {
-    return { x, y };
-};
-function selecionar(//'Chave' é um tipo genérico que extende 'Elementos'
-seletor) {
-    return document.querySelector(seletor);
+async function handleData() {
+    const jogo = await fetchData('/jogo.json');
+    if (checkInterface(jogo, 'desenvolvedora')) { //o valor do tipo genérico será a interface 'Jogo', o primeiro parâmetro será o objeto 'jogo' que veio do fetch e será verificado se o segundo parâmetro é uma das 'keys' dentro do objeto 'jogo' e da interface 'Jogo'
+        console.log(jogo.desenvolvedora);
+    }
+    const livro = await fetchData('/livro.json');
+    if (checkInterface(livro, 'autor')) {
+        console.log(livro.autor);
+    }
 }
-selecionar('body');
+handleData();
+function checkInterface(//faz a verificação de várias chaves passadas como parâmetro
+obj, ...keys
+//o valor da chave que eu passar para verificar se a interface possui a chave do objeto, deve ser uma 'keyof' da interface que está sendo verificada, caso  contrário, o TS vai avisar lá na função 'handleData'
+//foi usado o parâmetro 'rest' para que possa ser feita a verificação com mais de um chave
+) {
+    if (obj &&
+        typeof obj === 'object' &&
+        keys.filter((key) => key in obj).length === keys.length //verificando se todas as chaves passadas como parâmetro são chaves dentro do objeto passado como parâmetro
+    ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+function checkInterfaceSimples(//faz a verificação de apenas uma chave passada como parâmetro
+obj, key) {
+    if (obj && typeof obj === 'object' && key in obj) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
--- script.ts
+++ script.ts
@@ -1,37 +1,61 @@
-//---------------
-//keyof basicamente é um 'instanceof' para chaves de objetos
-interface Produto {
+async function fetchData<T>(url: string): Promise<T> { //esse tipo genérico não foi usado no código final...
+  const base = 'https://api.origamid.dev/json';
+  const response = await fetch(base + url);
+  return await response.json();
+}
+
+interface Jogo {
   nome: string;
-  preco: number;
-  novo: boolean;
+  ano: number;
+  desenvolvedora: string;
+  plataformas: string[];
 }
-let chave: keyof Produto;
-// let chave: "nome" | "produto";
-chave = 'nome';
-chave = 'preco';
 
-//---------------
-//typeof é nativo do JS, mas tbm temos o do TS, que serve para declarar o tipo
-function coordenadas(x: number, y: number) {
-  return { x, y };
+interface Livro {
+  nome: string;
+  ano: number;
+  autor: string;
+  paginas: number;
 }
-let coord: typeof coordenadas; //nesse caso, 'coord' virou uma arrow function auutomaticamente
-coord = (x: number, y: number) => {
-  return { x, y };
-};
 
-//---------------
-//querySelector (keyof no 'mundo real', quando utilizado em typeScript mais avançado)
-interface Elementos {
-  a: HTMLAnchorElement;
-  video: HTMLVideoElement;
-  div: HTMLElement;
-  body: HTMLBodyElement;
-  audio: HTMLAudioElement;
+async function handleData() {
+  const jogo = await fetchData('/jogo.json');
+  if (checkInterface<Jogo>(jogo, 'desenvolvedora')) { //o valor do tipo genérico será a interface 'Jogo', o primeiro parâmetro será o objeto 'jogo' que veio do fetch e será verificado se o segundo parâmetro é uma das 'keys' dentro do objeto 'jogo' e da interface 'Jogo'
+    console.log(jogo.desenvolvedora);
+  }
+
+  const livro = await fetchData('/livro.json');
+  if (checkInterface<Livro>(livro, 'autor')) {
+    console.log(livro.autor);
+  }
 }
-function selecionar<Chave extends keyof Elementos>( //'Chave' é um tipo genérico que extende 'Elementos'
-  seletor: Chave, //o parâmetro da função é do tipo 'Chave'
-): null | Elementos[Chave] { //o retorno da função será um dos valores no objeto 'Elemento', que possui a key correspondente à 'Chave'
-  return document.querySelector(seletor);
+
+handleData();
+
+function checkInterface<Interface>( //faz a verificação de várias chaves passadas como parâmetro
+  obj: unknown,
+  ...keys: Array<keyof Interface>
+  //o valor da chave que eu passar para verificar se a interface possui a chave do objeto, deve ser uma 'keyof' da interface que está sendo verificada, caso  contrário, o TS vai avisar lá na função 'handleData'
+  //foi usado o parâmetro 'rest' para que possa ser feita a verificação com mais de um chave
+): obj is Interface {
+  if (
+    obj &&
+    typeof obj === 'object' &&
+    keys.filter((key) => key in obj).length === keys.length //verificando se todas as chaves passadas como parâmetro são chaves dentro do objeto passado como parâmetro
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
-selecionar('body');
+
+function checkInterfaceSimples<Interface>( //faz a verificação de apenas uma chave passada como parâmetro
+  obj: unknown,
+  key: keyof Interface,
+): obj is Interface {
+  if (obj && typeof obj === 'object' && key in obj) {
+    return true;
+  } else {
+    return false;
+  }
+}
