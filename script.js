69df2476f4d4fc31b5bdf73d5f0506129892176a
Pedro <pedropaivadev@gmail.com>
2023-04-21 19:42:26 -0300

    class 403-1: keyword 'keyof' to indicate that data is a key of an interface and using TS 'typeof' to copy the interface of a function

--- script.js
+++ script.js
@@ -1,38 +1,19 @@
 "use strict";
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-async function fetchVendas0() {
-    const response = await fetch('https://api.origamid.dev/json/vendas.json');
-    const json = await response.json();
-    somaVendas(json);
+let chave;
+// let chave: "nome" | "produto";
+chave = 'nome';
+chave = 'preco';
+//---------------
+//typeof é nativo do JS, mas tbm temos o do TS, que serve para declarar o tipo
+function coordenadas(x, y) {
+    return { x, y };
 }
-function somaVendas(data) {
-    let soma = 0;
-    data.forEach((item) => {
-        soma += item[1];
-    });
-    document.body.innerHTML = `<h1>O somatório das vendas é de R$ ${soma.toFixed(2)}</h1>`;
+let coord; //nesse caso, 'coord' virou uma arrow function auutomaticamente
+coord = (x, y) => {
+    return { x, y };
+};
+function selecionar(//'Chave' é um tipo genérico que extende 'Elementos'
+seletor) {
+    return document.querySelector(seletor);
 }
-fetchVendas0();
-//----------------
-//RESOLUÇÃO do professor
-async function fetchVendas() {
-    const response = await fetch('https://api.origamid.dev/json/vendas.json');
-    const data = await response.json();
-    somarVendas(data);
-}
-function somarVendas(vendas) {
-    // Com for loop
-    let total1 = 0;
-    for (let i = 0; i < vendas.length; i++) {
-        total1 += vendas[i][1];
-    }
-    document.body.innerHTML += `<p>Total1: R$ ${total1}</p>`;
-    // Com reduce
-    const total2 = vendas.reduce((total, venda) => {
-        return total + venda[1];
-    }, 0);
-    document.body.innerHTML += `<p>Total2: R$ ${total2}</p>`;
-}
-fetchVendas();
+selecionar('body');
--- script.ts
+++ script.ts
@@ -1,56 +1,37 @@
-// 1 - Faça um fetch das vendas: https://api.origamid.dev/json/vendas.json
-// 2 - Defina o tipo/interface de cada venda (tuple)
-// 3 - Some o total das vendas e mostre na tela
-
-//----------------
-//EXERCÍCIO feito
+//---------------
+//keyof basicamente é um 'instanceof' para chaves de objetos
 interface Produto {
-  marca: string;
-  cor: string;
+  nome: string;
+  preco: number;
+  novo: boolean;
 }
-async function fetchVendas0() {
-  const response = await fetch('https://api.origamid.dev/json/vendas.json');
-  const json = await response.json();
-  somaVendas(json);
+let chave: keyof Produto;
+// let chave: "nome" | "produto";
+chave = 'nome';
+chave = 'preco';
+
+//---------------
+//typeof é nativo do JS, mas tbm temos o do TS, que serve para declarar o tipo
+function coordenadas(x: number, y: number) {
+  return { x, y };
 }
-function somaVendas(data: [any]) {
-  let soma: number = 0;
-  data.forEach((item: [string, number, string, Produto]) => {
-    soma += item[1]
-  })
-  document.body.innerHTML = `<h1>O somatório das vendas é de R$ ${soma.toFixed(2)}</h1>`;
+let coord: typeof coordenadas; //nesse caso, 'coord' virou uma arrow function auutomaticamente
+coord = (x: number, y: number) => {
+  return { x, y };
+};
+
+//---------------
+//querySelector (keyof no 'mundo real', quando utilizado em typeScript mais avançado)
+interface Elementos {
+  a: HTMLAnchorElement;
+  video: HTMLVideoElement;
+  div: HTMLElement;
+  body: HTMLBodyElement;
+  audio: HTMLAudioElement;
 }
-fetchVendas0();
-
-//----------------
-//RESOLUÇÃO do professor
-async function fetchVendas() {
-  const response = await fetch('https://api.origamid.dev/json/vendas.json');
-  const data = await response.json();
-  somarVendas(data);
+function selecionar<Chave extends keyof Elementos>( //'Chave' é um tipo genérico que extende 'Elementos'
+  seletor: Chave, //o parâmetro da função é do tipo 'Chave'
+): null | Elementos[Chave] { //o retorno da função será um dos valores no objeto 'Elemento', que possui a key correspondente à 'Chave'
+  return document.querySelector(seletor);
 }
-
-interface ProdutoDetalhes {
-  marca: string;
-  cor: string;
-}
-
-type Venda = [string, number, string, ProdutoDetalhes];
-
-function somarVendas(vendas: Venda[]) {
-  // Com for loop
-  let total1 = 0;
-  for (let i = 0; i < vendas.length; i++) {
-    total1 += vendas[i][1];
-  }
-  document.body.innerHTML += `<p>Total1: R$ ${total1}</p>`;
-
-  // Com reduce
-  const total2 = vendas.reduce((total, venda) => {
-    return total + venda[1];
-  }, 0);
-
-  document.body.innerHTML += `<p>Total2: R$ ${total2}</p>`;
-}
-
-fetchVendas();
+selecionar('body');
