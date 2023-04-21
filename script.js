5530c32872172f887dd24b1e53c82d7ca1fee7bc
Pedro <pedropaivadev@gmail.com>
2023-04-21 10:16:12 -0300

    class 310: object destructuring and '...rest' parameter

--- script.js
+++ script.js
@@ -1,42 +1,32 @@
 "use strict";
+// desestruturar objeto
+const { body } = document;
+function handleData({ nome, preco }) {
+    // a sintaxe após os ':' é amesma da interface. Outra alternativa seria criar a interface fora da função
+    nome.includes('book');
+    preco?.toFixed();
+}
+handleData({
+    nome: 'Notebook',
+    preco: 2000,
+});
 //---------------
-//'as'
-const video = document.querySelector('.player');
-// video.volume; // mostra os métodos disponíveis na produção, mas no runtime aponta um erro, pois não existe volume de null
-const link = document.querySelector('.link');
-async function fetchProduto() {
-    const response = await fetch('https://api.origamid.dev/json/notebook.json');
-    // return response.json() as Promise<Produto>; //referenciando o retorno da Promise como uma interface 'Produto', eu não preciso usar o 'as Produto' em nenhuma das handleProduto.
-    return response.json(); //devido a Promise não ser tipada aqui, eu preciso tipar com o 'as' em todas as handleProduto.
+//conhecer os dados
+function handleClick({ currentTarget, pageX, }) {
+    if (currentTarget instanceof HTMLElement) {
+        currentTarget.innerHTML = `<h1>Mouse Click em x:${pageX}</h1>`;
+    }
 }
-// Podemos usar o as em diferentes locais.
-async function handleProduto1() {
-    const produto = await fetchProduto();
-    console.log(produto.nome);
+document.documentElement.addEventListener('click', handleClick);
+//----------------
+//...rest
+function comparar(tipo, ...numeros) {
+    if (tipo === 'maior') {
+        return Math.max(...numeros);
+    }
+    if (tipo === 'menor') {
+        return Math.min(...numeros);
+    }
 }
-async function handleProduto2() {
-    const produto = (await fetchProduto());
-    console.log(produto.nome);
-}
-async function handleProduto3() {
-    const produto = await fetchProduto();
-    console.log(produto.nome);
-}
-handleProduto1();
-handleProduto2();
-handleProduto3();
-//---------------
-//'non-null'
-const videoA = document.querySelector('video'); //eu estou afirmando que o videoA nunca será null. Ou seja, eu tenho certeza que o elemento vai estar no HTML
-// videoA.volume; // mas aí ocorre o erro no runtime, pois não existe volume de null (videoA)
-document.querySelector('a').href = 'https://www.origamid.com'; //esse funciona, pq realmente tem um anchor no html
-//---------------
-//'Outras Sintaxes'
-const video1 = document.querySelector('.player');
-const video2 = document.querySelector('.player');
-const video3 = document.querySelector('.player'); // essa forma seria a mais segura, pois estamos apenas usando um tipo genérico do querySelector
-const video4 = document.querySelector('.player');
-video1.volume;
-video2.volume;
-video3?.volume;
-video4.volume;
+console.log(comparar('maior', 3, 2, 4, 30, 5, 6, 20));
+console.log(comparar('menor', 3, 2, 4, 1, 5, 6, 20));
--- script.ts
+++ script.ts
@@ -1,51 +1,39 @@
-//---------------
-//'as'
-const video = document.querySelector('.player') as HTMLVideoElement;
-// video.volume; // mostra os métodos disponíveis na produção, mas no runtime aponta um erro, pois não existe volume de null
-const link = document.querySelector('.link') as string;
-// erro TS, eu não posso referenciar um querySelector à uma 'string', pois precisa ser uma classe instanciada de 'Element', como o HTMLAnchorElement
+// desestruturar objeto
+const { body }: { body: HTMLElement } = document;
+function handleData({ nome, preco }: { nome: string; preco?: number }) {
+  // a sintaxe após os ':' é amesma da interface. Outra alternativa seria criar a interface fora da função
+  nome.includes('book');
+  preco?.toFixed();
+}
+handleData({
+  nome: 'Notebook',
+  preco: 2000,
+});
 
 //---------------
-//'any'
-interface Produto {
-  nome: string;
-  preco: number;
+//conhecer os dados
+function handleClick({
+  currentTarget,
+  pageX,
+}: {
+  currentTarget: EventTarget | null;
+  pageX: number;
+}) {
+  if (currentTarget instanceof HTMLElement) {
+    currentTarget.innerHTML = `<h1>Mouse Click em x:${pageX}</h1>`;
+  }
 }
-async function fetchProduto() {
-  const response = await fetch('https://api.origamid.dev/json/notebook.json');
-  // return response.json() as Promise<Produto>; //referenciando o retorno da Promise como uma interface 'Produto', eu não preciso usar o 'as Produto' em nenhuma das handleProduto.
-  return response.json(); //devido a Promise não ser tipada aqui, eu preciso tipar com o 'as' em todas as handleProduto.
-}
-// Podemos usar o as em diferentes locais.
-async function handleProduto1() {
-  const produto = await fetchProduto();
-  console.log(produto.nome);
-}
-async function handleProduto2() {
-  const produto = (await fetchProduto()) as Produto;
-  console.log(produto.nome);
-}
-async function handleProduto3() {
-  const produto = await fetchProduto();
-  console.log((produto as Produto).nome);
-}
-handleProduto1();
-handleProduto2();
-handleProduto3();
+document.documentElement.addEventListener('click', handleClick);
 
-//---------------
-//'non-null'
-const videoA = document.querySelector('video')!; //eu estou afirmando que o videoA nunca será null. Ou seja, eu tenho certeza que o elemento vai estar no HTML
-// videoA.volume; // mas aí ocorre o erro no runtime, pois não existe volume de null (videoA)
-document.querySelector('a')!.href = 'https://www.origamid.com';//esse funciona, pq realmente tem um anchor no html
-
-//---------------
-//'Outras Sintaxes'
-const video1 = document.querySelector('.player') as HTMLVideoElement;
-const video2 = <HTMLVideoElement>document.querySelector('.player');
-const video3 = document.querySelector<HTMLVideoElement>('.player');// essa forma seria a mais segura, pois estamos apenas usando um tipo genérico do querySelector
-const video4 = document.querySelector('.player');
-video1.volume;
-video2.volume;
-video3?.volume;
-(video4 as HTMLVideoElement).volume;
+//----------------
+//...rest
+function comparar(tipo: 'maior' | 'menor', ...numeros: number[]) {
+  if (tipo === 'maior') {
+    return Math.max(...numeros);
+  }
+  if (tipo === 'menor') {
+    return Math.min(...numeros);
+  }
+}
+console.log(comparar('maior', 3, 2, 4, 30, 5, 6, 20));
+console.log(comparar('menor', 3, 2, 4, 1, 5, 6, 20));
