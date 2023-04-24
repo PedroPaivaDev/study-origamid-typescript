c08e2d43b4880fbf0f86d81e8d266af94930b2d5
Pedro <pedropaivadev@gmail.com>
2023-04-24 12:29:46 -0300

    exercise: almost finished, just need to show the day of the week with more sales

--- src/fetchData.ts
+++ src/fetchData.ts
@@ -1,5 +1,26 @@
 export default async function fetchData() {
   const response = await fetch('https://api.origamid.dev/json/transacoes.json');
   const json = await response.json();
-  return json;
+  return normalizeData(json);
+}
+
+function normalizeData(data: Array<object>) {
+  let newData:Array<Venda> = []
+  data.forEach((element:any) => {
+    const keys = Object.keys(element)
+    newData = [
+      ...newData,
+      {
+        status: String(element[keys[0]]),
+        id: Number(element[keys[1]]),
+        data: String(element[keys[2]]),
+        nome: String(element[keys[3]]),
+        pagamento: String(element[keys[4]]),
+        email: String(element[keys[5]]),
+        valor: element[keys[6]],
+        novo: element[keys[7]]===1 ? true : false
+      }
+    ]
+  });
+  return newData;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,104 @@
 import './style.css';
 import fetchData from './fetchData';
 
-// interface Venda {
-//   status: string,
-//   id: number,
-//   data: string,
-//   nome: string,
-//   pagamento: "Boleto" | "Cartão de Crédito",
-//   email: string,
-//   valor: number,
-//   novo: boolean
-// }
-
-async function normalizeData() {
-  const data:Array<object> = await fetchData()
-  data.forEach((element:any) => {
-    const keys = Object.keys(element)
-    const newData = {
-      status: String(element[keys[0]]),
-      id: Number(element[keys[1]]),
-      data: String(element[keys[2]]),
-      nome: String(element[keys[3]]),
-      pagamento: String(element[keys[4]]),
-      email: String(element[keys[5]]),
-      valor: Number(element[keys[6]]),
-      novo: element[keys[7]]===1 ? true : false
-    }
-    console.log(newData)    
-  });
+declare global {
+  interface Venda {
+    status: string,
+    id: number,
+    data: string,
+    nome: string,
+    pagamento: string,
+    email: string,
+    valor: string,
+    novo: boolean
+  }
 }
 
-normalizeData()
+async function mostrarDados() {
+  const data:Array<Venda> = await fetchData();
+  mostrarTransacoes(data);
+  mostrarTotal(data);
+  mostrarPagamentos(data);
+  mostrarStatus(data);
+  mostrarDiaTopVendas(data);
+}
+mostrarDados()
 
+function mostrarTransacoes(data:Array<Venda>) {
+  const app = document.getElementById('transacoesBody');
+  data.forEach(element => {
+    if(app) app.innerHTML += `
+      <tr>
+        <td>${element.nome}</td>
+        <td>${element.email}</td>
+        <td>R$ ${element.valor}</td>
+        <td>${element.pagamento}</td>
+        <td>${element.status}</td>
+      </tr>
+    `
+  })
+}
+
+function mostrarTotal(data:Array<Venda>) {
+  const total = document.getElementById('total');
+  let soma:number = 0;
+  data.forEach((element) => {
+    if(parseInt(element.valor.replace('.', ''))) {
+      soma += parseInt(element.valor.replace('.', ''));
+    }
+  });
+  if(total) total.innerText += ` R$ ${soma}`;
+}
+
+function mostrarPagamentos(data:Array<Venda>) {
+  const pagamento = document.getElementById('pagamento');
+  let credito:number = 0;
+  let boleto:number = 0;
+  data.forEach((element) => {
+    if(element.pagamento === 'Cartão de Crédito') {
+      credito += 1
+    } else {
+      boleto += 1
+    }
+  });
+  if(pagamento) pagamento.innerHTML = `
+    <p>Cartão de Crédito: ${credito}</p>
+    <p>Boleto: ${boleto}</p>
+  `;
+}
+
+function mostrarStatus(data:Array<Venda>) {
+  const status = document.getElementById('status');
+  let paga:number = 0;
+  let recusada:number = 0;
+  let aguardando:number = 0;
+  let estornada:number = 0;
+  data.forEach((element) => {
+    if(element.status === 'Paga') {
+      paga += 1
+    } else if(element.status === 'Recusada pela operadora de cartão') {
+      recusada += 1
+    } else if(element.status === 'Aguardando pagamento') {
+      aguardando += 1
+    } else if(element.status === 'Estornada') {
+      estornada += 1
+    }
+  });
+  if(status) status.innerHTML = `
+    <p>Paga: ${paga}</p>
+    <p>Recusada pela operadora de cartão: ${recusada}</p>
+    <p>Aguardando pagamento: ${aguardando}</p>
+    <p>Estornada: ${estornada}</p>
+  `;
+}
+
+function mostrarDiaTopVendas(data:Array<Venda>) {
+  const dia = document.getElementById('dia');
+  let topDayObject:object = data.reduce((prev, curr) => {
+    return {
+      ...prev,
+      [curr.data]: curr.data+1
+    }
+  }, {});
+  if(dia) dia.innerText += ` ${Object.keys(topDayObject)}`;
+}
