041abbd29e310fdc82100005fe66c0003ad30524
Pedro <pedropaivadev@gmail.com>
2023-04-26 20:12:32 -0300

    resolution: created a generic function to receive an array of string or number and return an object with the name and number of times an element is repeated in the array

--- src/Estatisticas.ts
+++ src/Estatisticas.ts
@@ -1,3 +1,5 @@
+import countBy from "./countBy";
+
 type VendaValor = Venda & {valor:number};
 
 function filtrarValor(transacao: Venda): transacao is VendaValor {
@@ -7,13 +9,27 @@ function filtrarValor(transacao: Venda): transacao is VendaValor {
 export default class Estatisticas {
   private transacoes;
   total;
+  pagamento;
+  status;
+
   constructor(transacoes: Venda[]) {
     this.transacoes = transacoes;
     this.total = this.setTotal();
+    this.pagamento = this.setPagamento();
+    this.status = this.setStatus();
   }
+
   private setTotal() {
     return this.transacoes.filter(filtrarValor).reduce((acc, item) => {
       return acc + item.valor;
     }, 0);
+  }
+
+  private setPagamento() {
+    return countBy(this.transacoes.map(({pagamento}) => pagamento));
+  }
+
+  private setStatus() {
+    return countBy(this.transacoes.map(({status}) => status));
   }
 }
